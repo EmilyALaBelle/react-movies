1788949386026026c0355a3340118f2fd8fde199
EmilyALaBelle <emilylabelle8@gmail.com>
2022-10-27 15:09:37 -0400

    redddddd

---
+++ bocacode/react/react-movies/src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+function Header() {  //can also add export default to this line instead
+    return (
+        <header>
+            <h1>React Movies</h1>
+        </header>
+    )
+}
+
+export default Header; //Does the same thing as adding export default infront of function
