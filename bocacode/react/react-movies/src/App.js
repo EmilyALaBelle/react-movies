86cb50d5c66a1da730d88254971dd80ddf021f9f
EmilyALaBelle <emilylabelle8@gmail.com>
2022-10-27 15:19:28 -0400

    movies file

--- bocacode/react/react-movies/src/App.js
+++ bocacode/react/react-movies/src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header';
+import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MoviesList />
     </div>
   );
 }
--- bocacode/react/react-movies/src/components/moviesList.jsx
+++ bocacode/react/react-movies/src/components/moviesList.jsx
@@ -1,1 +1,11 @@
 import movies from '../movies'
+
+function moviesList() {
+    return (
+        <section>
+            <h2>Movies go here...</h2>
+        </section>
+    )
+}
+
+export default moviesList;
