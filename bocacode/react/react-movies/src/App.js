da92b8a240d84ea1b475172b776bb682f8e2dc4c
EmilyALaBelle <emilylabelle8@gmail.com>
2022-10-27 16:07:10 -0400

    footer

--- bocacode/react/react-movies/src/App.js
+++ bocacode/react/react-movies/src/App.js
@@ -1,4 +1,5 @@
 import Header from './components/Header';
+import Footer from './components/Footer'
 import MoviesList from './components/MoviesList';
 import './App.css';
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Header />
       <MoviesList />
+      <Footer />
     </div>
   );
 }
---
+++ bocacode/react/react-movies/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; 2022 Emily LaBelle. 
+                Source: <a href="https://sampleapis.com/api-list/movies">
+                    sampleapis.com
+                    </a> </p>
+        </footer>
+    )
+}
+
+export default Footer;
